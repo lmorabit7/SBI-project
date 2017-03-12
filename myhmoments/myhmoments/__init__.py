b5fa3b566ee38d9468f591c5204a4bf37eed801f
lmorabit7 <lauramorabitria@gmail.com>
2017-03-12 19:52:46 +0100

    Delete __init__.py

--- myhmoments/myhmoments/__init__.py
+++
@@ -1,9 +0,0 @@
-#__all__= ['exceptions','color_scales', 'colors', 'hphob_scales', 'surface', 'moments', 'calculator']
-
-from .color_scales import *
-from .colors import *
-from .exceptions import *
-from .hphob_scales import *
-from .surface import *
-from .moments import *
-from .__main__ import *
